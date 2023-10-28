0abb1bb9606d618c2da1cb6bf584060b6671a3ce
Sachin Arya <sachinkarya333@gmail.com>
2023-10-28 15:39:35 +0530

    feat: use location obj for path change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,16 @@ import Notification from "../assets/images/notifications-outline-icon.webp";
 import User from "../assets/images/user-icon.webp";
 
 const Navbar = () => {
+
     return (
         <div className="flex justify-between h-16 shadow-lg">
             <div className="flex">
                 <button className=""><img src={HAMBURGER_MENU} alt="hamburger-menu" className="h-8 mx-4" /></button>
-                <img src={LOGO} alt="logo" className="" />
+                <img src={LOGO} alt="logo" className="cursor-pointer" onClick={() => window.location.pathname = "/"} />
             </div>
             <div className="flex">
-                <img src={Notification} alt="notification" className="h-8 mt-4 mr-6" />
-                <img src={User} alt="user" className="h-8 mt-4 mr-6" />
+                <button><img src={Notification} alt="notification" className="h-8  mr-6" /></button>
+                <button><img src={User} alt="user" className="h-8  mr-6" /></button>
             </div>
         </div>
     );
